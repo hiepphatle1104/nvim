a788ae64733fa914da338976648e083761f62f9d
hphats <phatlee1104@gmail.com>
2025-04-05 22:38:15 +0700

    update mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -29,7 +29,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "ts_ls" },
+				ensure_installed = { "lua_ls", "gopls", "ts_ls", "jdtls", "groovyls", "pyright" },
 				automatic_installation = true,
 			})
 		end,
