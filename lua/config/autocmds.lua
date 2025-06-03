ff2fdc4642883b4cb91ae7afb120b004b6a31ab5
hphats <phatlee1104@gmail.com>
2025-06-03 18:07:09 +0700

    fix: makefile syntax highlighting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,34 +1,34 @@
 -- Floating Help
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*.txt",
-	callback = function()
-		if vim.bo.filetype == "help" then
-			local buf = vim.api.nvim_get_current_buf()
-			vim.cmd("wincmd c")
-			vim.api.nvim_open_win(buf, true, {
-				relative = "editor",
-				width = math.floor(vim.o.columns * 0.8),
-				height = math.floor(vim.o.lines * 0.8),
-				col = math.floor(vim.o.columns * 0.1),
-				row = math.floor(vim.o.lines * 0.1),
-				border = "rounded",
-			})
-			vim.cmd("setlocal nocursorline nonumber norelativenumber")
+  pattern = "*.txt",
+  callback = function()
+    if vim.bo.filetype == "help" then
+      local buf = vim.api.nvim_get_current_buf()
+      vim.cmd("wincmd c")
+      vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+        col = math.floor(vim.o.columns * 0.1),
+        row = math.floor(vim.o.lines * 0.1),
+        border = "rounded",
+      })
+      vim.cmd("setlocal nocursorline nonumber norelativenumber")
 
-			vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-		end
-	end,
+      vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end
+  end,
 })
 
 -- Yank Highlight
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "Visual", -- Nhóm highlight để sử dụng
-			timeout = 200, -- Thời gian highlight (ms)
-			on_macro = true, -- Hỗ trợ highlight khi dùng macro
-		})
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "Visual", -- Nhóm highlight để sử dụng
+      timeout = 200, -- Thời gian highlight (ms)
+      on_macro = true, -- Hỗ trợ highlight khi dùng macro
+    })
+  end,
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,26 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"go",
-				"gomod",
-				"gowork",
-				"gosum",
-				"java",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"python",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "java",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "python",
+        "make",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
