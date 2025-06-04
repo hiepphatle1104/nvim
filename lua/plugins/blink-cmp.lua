1c466f80206a808394aeea3e5d2311bef3155e34
hphats <phatlee1104@gmail.com>
2025-06-04 11:56:52 +0700

    update: add codeium and fix blink cmp doc background

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,59 +1,66 @@
 return {
-	{
-		"saghen/blink.compat",
-		version = "*",
-		lazy = true,
-		opts = {},
-	},
-	{
-		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets", "nvim-web-devicons", "onsails/lspkind.nvim" },
-		version = "1.*",
-		event = "InsertEnter",
-		opts = {
-			completion = {
-				menu = {
-					border = "rounded",
-					draw = {
-						columns = {
-							{ "label", "label_description", gap = 1 },
-							{ "kind_icon", "kind", gap = 1 },
-						},
-						components = {
-							kind_icon = {
-								text = function(ctx)
-									local lspkind = require("lspkind")
-									local icon = ctx.kind_icon
-									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-										if dev_icon then
-											icon = dev_icon
-										end
-									else
-										icon = lspkind.symbolic(ctx.kind, {
-											mode = "symbol",
-										})
-									end
+  {
+    "saghen/blink.compat",
+    version = "*",
+    lazy = true,
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "nvim-web-devicons",
+      "onsails/lspkind.nvim",
+      "Exafunction/codeium.nvim",
+    },
+    version = "1.*",
+    event = "InsertEnter",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+          draw = {
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon", "kind", gap = 1 },
+            },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local lspkind = require("lspkind")
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = lspkind.symbolic(ctx.kind, {
+                      mode = "symbol",
+                    })
+                  end
 
-									return icon .. ctx.icon_gap
-								end,
-							},
-						},
-					},
-				},
-				documentation = {
-					window = { border = "rounded" },
-					auto_show = true,
-					auto_show_delay_ms = 500,
-				},
-			},
-			signature = { enabled = true },
-			keymap = { preset = "enter", ["<C-k>"] = {} },
+                  return icon .. ctx.icon_gap
+                end,
+              },
+            },
+          },
+        },
+        documentation = {
+          window = { border = "rounded" },
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
+      signature = { enabled = true },
+      keymap = { preset = "enter", ["<C-k>"] = {} },
 
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-				providers = {},
-			},
-		},
-	},
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "codeium" },
+        providers = {
+          codeium = { name = "Codeium", module = "codeium.blink", async = true },
+        },
+      },
+    },
+  },
 }
